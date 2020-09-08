a10d4d7db3810e18662fbea3922bea079c4e840b
Yesid <trigoperfec3@hotmail.com>
2020-09-08 16:15:05 -0500

    Adicion de animaciones cuando cambia entre fragments

--- app/src/main/java/com/mitiempo/pruebamuy/Utilidades/Extensiones/ExtensionesAppCompatActivity.kt
+++ app/src/main/java/com/mitiempo/pruebamuy/Utilidades/Extensiones/ExtensionesAppCompatActivity.kt
@@ -3,11 +3,13 @@ package com.mitiempo.pruebamuy.Utilidades.Extensiones
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.mitiempo.pruebamuy.R
 
 fun AppCompatActivity.navegarAFragment(@IdRes idContenedor : Int, fragment : Fragment,tag : String? = null ){
     runOnUiThread {
         supportFragmentManager
             .beginTransaction()
+            .setCustomAnimations(R.animator.fade_in,R.animator.fade_out,R.animator.fade_in,R.animator.fade_out)
             .replace(idContenedor,fragment)
             .addToBackStack(tag)
             .commit()
